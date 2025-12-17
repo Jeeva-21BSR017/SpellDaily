d9cce456d25a6635c27944159d09a5ff20ffc383
Jeeva-Zenthink <jeeva.zenthink@gmail.com>
2025-12-17 21:05:42 +0530

    fix : json issue

---
+++ data.js
@@ -0,0 +1,167 @@
+const appData = {
+    "user": {
+        "name": "Jeeva",
+        "streakDays": 36,
+        "streakMessage": "days - unstoppable consistency !",
+        "rankTitle": "Top 25",
+        "percentileText": "is in the top 35% of learners",
+        "learningRate": {
+            "newWordsCount": 66,
+            "timeFrame": "coming month"
+        }
+    },
+    "summary": {
+        "practiced": 62,
+        "correctFirstAttempt": 27
+    },
+    "wordBank": [
+        {
+            "word": "Foreign",
+            "correct": true
+        },
+        {
+            "word": "Opinion",
+            "correct": true
+        },
+        {
+            "word": "Angry",
+            "correct": true
+        },
+        {
+            "word": "Pillow",
+            "correct": false
+        },
+        {
+            "word": "Simple",
+            "correct": true
+        },
+        {
+            "word": "Temple",
+            "correct": true
+        },
+        {
+            "word": "Mirror",
+            "correct": false
+        },
+        {
+            "word": "Music",
+            "correct": false
+        },
+        {
+            "word": "Children",
+            "correct": true
+        },
+        {
+            "word": "Dancer",
+            "correct": true
+        },
+        {
+            "word": "Evening",
+            "correct": true
+        },
+        {
+            "word": "Group",
+            "correct": false
+        },
+        {
+            "word": "Stories",
+            "correct": false
+        },
+        {
+            "word": "Vegetable",
+            "correct": false
+        },
+        {
+            "word": "Apartment",
+            "correct": false
+        },
+        {
+            "word": "Autumn",
+            "correct": false
+        },
+        {
+            "word": "Believe",
+            "correct": false
+        },
+        {
+            "word": "Fifteen",
+            "correct": true
+        },
+        {
+            "word": "Bicycle",
+            "correct": false
+        },
+        {
+            "word": "Business",
+            "correct": false
+        },
+        {
+            "word": "Career",
+            "correct": false
+        },
+        {
+            "word": "Celebrate",
+            "correct": true
+        },
+        {
+            "word": "Change",
+            "correct": true
+        },
+        {
+            "word": "College",
+            "correct": false
+        },
+        {
+            "word": "Compare",
+            "correct": true
+        },
+        {
+            "word": "Complete",
+            "correct": true
+        },
+        {
+            "word": "Create",
+            "correct": true
+        },
+        {
+            "word": "Conversation",
+            "correct": false
+        },
+        {
+            "word": "Correct",
+            "correct": false
+        },
+        {
+            "word": "Country",
+            "correct": false
+        },
+        {
+            "word": "Culture",
+            "correct": false
+        },
+        {
+            "word": "Dangerous",
+            "correct": false
+        },
+        {
+            "word": "Decide",
+            "correct": false
+        },
+        {
+            "word": "Delicious",
+            "correct": false
+        },
+        {
+            "word": "Describe",
+            "correct": false
+        },
+        {
+            "word": "Difference",
+            "correct": false
+        },
+        {
+            "word": "Difficult",
+            "correct": false
+        }
+    ]
+};
